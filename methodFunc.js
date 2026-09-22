0dc82bd4c353cb1ff399577040af87ff03922ffa
kimani-25-kikis <kimanikikis@gmail.com>
2026-09-22 23:40:36 +0300

    functions

--- methodFunc.js
+++ methodFunc.js
@@ -9,11 +9,17 @@
 
 // person.introduce();
 
-const person ={
-  name :"Joshua",
-    course: "Computer Science",
-    introduce: function(){
-        console.log( `my name is ${this.name} pursuing ${this.course}`)
-  }
+// const person ={
+//   name :"Joshua",
+//     course: "Computer Science",
+//     introduce: function(){
+//         console.log( `my name is ${this.name} pursuing ${this.course}`)
+//   }
+// }
+// person.introduce()
+
+function student(name,age){
+  return {name, age}
 }
-person.introduce()
+let person = student("Josh", 25);
+console.log(person)
