6d1a6f55ecb24f5754334e11946b887d758dd714
kimani-25-kikis <kimanikikis@gmail.com>
2026-09-22 23:21:32 +0300

    Methods invoke

--- methodFunc.js
+++ methodFunc.js
@@ -1,10 +1,19 @@
-const person = {
-  name: "Josh",
-  age: 30,
+// const person = {
+//   name: "Josh",
+//   age: 30,
 
-  introduce: function() {
-    console.log(`My name is ${this.name} and I am ${this.age} years old.`);
+//   introduce: function() {
+//     console.log(`My name is ${this.name} and I am ${this.age} years old.`);
+//   }
+// };
+
+// person.introduce();
+
+const person ={
+  name :"Joshua",
+    course: "Computer Science",
+    introduce: function(){
+        console.log( `my name is ${this.name} pursuing ${this.course}`)
   }
-};
-
-person.introduce();
+}
+person.introduce()
