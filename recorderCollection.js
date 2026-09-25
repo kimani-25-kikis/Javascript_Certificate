a53a54773a890d0907c7531f26647e9de9f67129
kimani-25-kikis <kimanikikis@gmail.com>
2026-09-25 21:44:34 +0300

    Recorder collection

---
+++ recorderCollection.js
@@ -0,0 +1,14 @@
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks") {
+    records[id][prop] = value;
+  } else if (!records[id].hasOwnProperty("tracks")) {
+    records[id].tracks = [];
+    records[id].tracks.push(value);
+  } else {
+    records[id].tracks.push(value);
+  }
+
+  return records;
+}
